2b1f735f4fa4da570528f57d3b52badfacf880e2
yzs0331 <yaozs@magima.com.cn>
2020-11-02 16:22:11 +0800

    重命名文件夹

---
+++ Week_03/02-Gateway2.0/nio02/src/main/java/io/github/kimmking/gateway/outbound/okhttp/OkHttpOutboundHandler.java
@@ -0,0 +1,97 @@
+package io.github.kimmking.gateway.outbound.okhttp;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class OkHttpOutboundHandler {
+
+    private OkHttpClient okHttpClient;
+    private Call call;
+    private String backendUrl;
+
+    public OkHttpOutboundHandler(String backendUrl) {
+        this.backendUrl = backendUrl.endsWith("/")?backendUrl.substring(0,backendUrl.length()-1):backendUrl;
+        // 创建OkHttpClient对象
+        okHttpClient = new OkHttpClient();
+    }
+
+    public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) {
+        final String url = this.backendUrl + fullRequest.uri();
+        fetchGet(fullRequest, ctx, url);
+    }
+
+    private void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
+        // 创建Request对象
+        Request request = new Request.Builder().url(url).get().build();
+        // 将Request 对象封装为Call
+        call = okHttpClient.newCall(request);
+        Response endpointResponse = null;
+        try {
+            // 通过Call调用execute方法同步执行
+            endpointResponse = call.execute();
+            // 处理请求响应内容
+            handleResponse(inbound, ctx, endpointResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final Response endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+//            String value = "hello,kimmking";
+//            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+//            response.headers().set("Content-Type", "application/json");
+//            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+
+            byte[] body = endpointResponse.body().string().getBytes();
+            System.out.println("OKHttp:"+new String(body));
+//            System.out.println(body.length);
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", (int) endpointResponse.body().contentLength());
+
+//            for (Header e : endpointResponse.getAllHeaders()) {
+//                //response.headers().set(e.getName(),e.getValue());
+//                System.out.println(e.getName() + " => " + e.getValue());
+//            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    //response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+            //ctx.close();
+        }
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
